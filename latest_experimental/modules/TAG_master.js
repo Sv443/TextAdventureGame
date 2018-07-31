ecea6ae2ad890646e7a1e112d20fc26c3ef02beb
Sv443 <38158426+Sv443@users.noreply.github.com>
2018-07-31 19:22:47 +0200

    Update TAG_master.js

--- latest_experimental/modules/TAG_master.js
+++ latest_experimental/modules/TAG_master.js
@@ -17,10 +17,10 @@ var curversion = "[B]-0.0.5";
 var death_enabled = true; //enable / disable death - default true
 var max_volume = 0.2; //maximum volume level of played audios - default and recommended 0.2 - min 0 - max 1
 var dbg = false; //debug to the html output - default false
-var devmenu = true; //enable developer menu - default false
+var devmenu = false; //enable developer menu - default false
 var module_load_time = 500; //time buffer for the modules to load / reload. increase if they don't load correctly or decrease if they load too slowly
-var daynightcycle_delay = 3000; /*time in ms between the day/night cycle states (12 states/day) (entered time in seconds * 12 / 60 = duration of a full day/night cycle) (or uncomment the following and look in the console to see the duration)*/       //console.log("daynightcycle - full cycle duration: " + daynightcycle_delay/1000*12/60 + " minutes or " + daynightcycle_delay/1000*12 + " seconds");
-var unlock_all_timed = true; //unlocks all positive timed events (eg. finding the wreck) - default false
+var daynightcycle_delay = 30000; /*time in ms between the day/night cycle states (12 states/day) (entered time in seconds * 12 / 60 = duration of a full day/night cycle) (or uncomment the following and look in the console to see the duration)*/       //console.log("daynightcycle - full cycle duration: " + daynightcycle_delay/1000*12/60 + " minutes or " + daynightcycle_delay/1000*12 + " seconds");
+var unlock_all_timed = false; //unlocks all positive timed events (eg. finding the wreck) - default false
 var test_item_container = false; //adds a placeholder item every second to test item container - default false
 
 var key_send = 13; //key to send action - default 13 (enter)
